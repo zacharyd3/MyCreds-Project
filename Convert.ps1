8a2b3c7780ff4ebb5009481bdf0d1fce0b8ce80a
zacharyd3 <zacharyd3@hotmail.com>
2022-10-07 15:28:54 -0400

    Updated to properly handle CSV files
    Previously, it was only setup to handle multiple CSV files and add all the rows to 1 XML, now I've updated the code so it handles multiple CSV's and only creates one XML per row (one user is stored per row).

--- Convert.ps1
+++ Convert.ps1
@@ -1,5 +1,5 @@
 ##Created by Zach Dokuchic at Oshki-Wenjack for use with the MyCreds project.
-
+cls
 # Folder containing source CSV files
 $folderPath = 'C:\IT\XML Conversion\Source CSV'
 
@@ -17,61 +17,80 @@ $xmlTemplate = @"
 # and also a template for the individual <Person> nodes
 # inside are placeholders '{0}' we will fill in later
 $studentTemplate = @"
-    <Person> 
-		<SchoolAssignedPersonID>{0}</SchoolAssignedPersonID > 
-		<Birth> 
-			<BirthDate>{1}</Birthdate> 
-		</Birth> 
-		<Name> 
-			<FirstName>{2}</FirstName> 
-			<LastName>{3}</LastName> 
-		</Name> 
-		<Contacts> 
-			<Email>{4}</Email> 
-		</Contacts> 
-	</Person> 
-	<LearningRecord> 
-		<MicrocredentialAward> 
-			<AwardID>{5}</AwardID> 
-			<AwardLevel>{6}</AwardLevel> 
-			<AwardTitle>{7}</AwardTitle> 
-			<AwardDescription>{8}</AwardDescription> 
-			<AwardHonours>{9}</AwardHonours> 
-			<MicrocredentialAwardProgram> 
-				<ProgramName>{10}</ProgramName> 
-			</MicrocredentialAwardProgram> 
-			<AwardDate>{11}</AwardDate 
-			<IssuedDate>{12}</IssuedDate> 
-		</MicrocredentialAward> 
-		<MicrocredentialOrganization> 
-			<IssuingBodyID>{13}</IssuingBodyID> 
-			<IssuingBodyName>{14}</IssuingBodyName> 
-			<Contacts> 
-				<Address> 
-					<CountryCode>{15}</CountryCode> 
-				</Address> 
-			<URL>{16}</URL> 
-			</Contacts> 
-		</MicrocredentialOrganization> 
-	</LearningRecord> 
+    <Person>
+		<SchoolAssignedPersonID>{0}</SchoolAssignedPersonID>
+		<Birth>
+			<BirthDate>{1}</BirthDate>
+		</Birth>
+		<Name>
+			<FirstName>{2}</FirstName>
+			<LastName>{3}</LastName>
+		</Name>
+		<Contacts>
+			<Email>{4}</Email>
+		</Contacts>
+	</Person>
+	<LearningRecord>
+		<MicrocredentialAward>
+			<AwardID>{5}</AwardID>
+			<AwardLevel>{6}</AwardLevel>
+			<AwardTitle>{7}</AwardTitle>
+			<AwardDescription>{8}</AwardDescription>
+			<AwardHonours>{9}</AwardHonours>
+			<MicrocredentialAwardProgram>
+				<ProgramName>{10}</ProgramName>
+			</MicrocredentialAwardProgram>
+			<AwardDate>{11}</AwardDate>
+			<IssuedDate>{12}</IssuedDate>
+		</MicrocredentialAward>
+		<MicrocredentialOrganization>
+			<IssuingBodyID>{13}</IssuingBodyID>
+			<IssuingBodyName>{14}</IssuingBodyName>
+			<Contacts>
+				<Address>
+					<CountryCode>{15}</CountryCode>
+				</Address>
+			<URL>{16}</URL>
+			</Contacts>
+		</MicrocredentialOrganization>
+	</LearningRecord>
 "@
 
+$itemNumber=0
 # Generate a list of all files in the folder and pipe it to ForEach-Object
-Get-ChildItem -Path $folderPath -Filter '*.csv' -File | ForEach-Object {
-    
-	# Combines destination path and file name with extension .xml
-    $filePathdest = Join-Path -Path $folderPathDest -ChildPath ('{0}.xml' -f $_.BaseName)
-	
+Get-ChildItem -Path $folderPath -Filter '*.csv' -File | ForEach-Object {  	
+   
     # Import the CSV file
-    $data  = Import-Csv -Path $_.FullName
-    $Students = foreach ($Student in $data) {
-        # output a <Student> section with placeholders filled in
-        $studentTemplate -f $Student.SchoolAssignedPersonID, $Student.BirthDate, $Student.FirstName, $Student.LastName, $Student.Email, $Student.AwardID, $Student.AwardLevel, $Student.AwardTitle, $Student.AwardDescription, $Student.AwardHonours, $Student.ProgramName, $Student.AwardDate, $Student.IssuedDate, $Student.IssuingBodyID, $Student.IssuingBodyName, $Student.CountryCode, $Student.URL
+    $data = Import-Csv -Path $_.FullName
+
+	$xmlOutput = foreach ($Student in $data) 
+    {
+		$studentTemplate -f $Student.SchoolAssignedPersonID, $Student.BirthDate, $Student.FirstName, $Student.LastName, $Student.Email, $Student.AwardID, $Student.AwardLevel, $Student.AwardTitle, $Student.AwardDescription, $Student.AwardHonours, $Student.ProgramName, $Student.AwardDate, $Student.IssuedDate, $Student.IssuingBodyID, $Student.IssuingBodyName, $Student.CountryCode, $Student.URL
     }
-    
-	# create the completed XML and write this to file
-    $xmlTemplate -replace '@@STUDENTNODES@@', ($Students -join [environment]::NewLine) | Set-Content -Path $filePathdest -Encoding utf8
-}
 
+    # Outputs the total number of rows found (debugging)
+    Write-Output (-join('Total Rows: ',$xmlOutput.count))
 
-	
+    # Iterates through the xmlOutput array
+    while ($itemNumber -lt ($xmlOutput.count))
+	{
+        
+        # Outputs which row is currently being checked
+        Write-Output (-join('Checking Row: ',($itemNumber+1)))
+
+        # Combines destination path and file name with extension .xml
+	    $filePathdest = (-join($folderPathDest,'\',$data[$itemNumber].SchoolAssignedPersonID,'.xml'))
+        
+        # Generate and save the XML if there is only 1 row, the variable stays a string the if statement manipulates it based on those details.
+        if ($xmlOutput.Count -eq 1){
+            $xmlTemplate -replace '@@STUDENTNODES@@',$xmlOutput | Set-Content -Path $filePathdest -Encoding utf8
+        }
+        if ($xmlOutput.Count -gt 1){
+            $xmlTemplate -replace '@@STUDENTNODES@@',$xmlOutput[$itemNumber] | Set-Content -Path $filePathdest -Encoding utf8
+        }
+
+        # Output the log as files are generated
+        Write-Output (-join('XML generated for ',$data[$itemNumber].FirstName,' ',$data[$itemNumber].LastName,', save location = ',$folderPathDest,'\',$data[$itemNumber].SchoolAssignedPersonID,'.xml','.'))
+        $itemNumber = $itemNumber + 1
+    }
+}	
